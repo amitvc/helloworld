95dcdf901bf1ac86316ba0d6ce54b9ce73acba67
amitvc <achavan1@gmail.com>
2015-10-18 15:55:48 -0400

    Added new route to get Dept by dept Id

--- src/main/java/com/adheris/controllers/OrganizationController.java
+++ src/main/java/com/adheris/controllers/OrganizationController.java
@@ -2,6 +2,7 @@ package com.adheris.controllers;
 
 import java.io.File;
 
+import javax.websocket.server.PathParam;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
@@ -10,8 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adheris.model.Department;
 import com.adheris.model.Organization;
 
 @RestController
@@ -28,6 +31,25 @@ public class OrganizationController {
 			ex.printStackTrace();
 			return new ResponseEntity (ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+	
+	// localhost:8080/department?deptId=xyz
+	@RequestMapping(value= "/department", method = RequestMethod.GET)
+	public ResponseEntity<Department> getDepartment(@RequestParam("deptId") String deptId) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(Organization.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			Organization org = (Organization) unmarshaller.unmarshal(new File ("config.xml"));
+			for(Department dept : org.getDepartments()) {
+				if(dept.getName().equalsIgnoreCase(deptId)) {
+					return new ResponseEntity<Department>(dept, HttpStatus.OK);
+				}
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			return new ResponseEntity (ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return null;
 	}
 	
 }
